e66390cd6ec89c0c93c3f366f5c02589da88a437
tocomi <tocomi0112@gmail.com>
2026-07-27 19:18:50 +0900

    auto-suggestions settings

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ ln -s $HOME/dotfiles/.zsh_common $HOME/.zsh_common
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/config.ghostty "$HOME/Library/Application Support/com.mitchellh.ghostty/config.ghostty"
 
+# .zsh_common が参照するパッケージ
+brew install zsh-autosuggestions
+
