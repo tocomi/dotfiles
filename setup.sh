1174233bceedc3dc8e09d7a55d8ad393be03aece
tocomi <tocomi0112@gmail.com>
2026-07-27 15:29:11 +0900

    completion

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
+ln -s $HOME/dotfiles/.zsh_common $HOME/.zsh_common
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/config.ghostty "$HOME/Library/Application Support/com.mitchellh.ghostty/config.ghostty"
 
