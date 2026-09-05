4c875894fa45ab54fa8b10e42452b68dc67e40db
tocomi <tocomi0112@gmail.com>
2026-09-05 17:42:28 +0900

    ghq + peco

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/config.ghostty "$HOME/Library/Application Support/com.mitchellh.ghostty/config.ghostty"
 
 # .zsh_common が参照するパッケージ
-brew install zsh-autosuggestions
+brew install zsh-autosuggestions ghq peco
 
